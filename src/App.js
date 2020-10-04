738adf84092721ada8ed16cc5bba2c992e61b6d0
Ali Karani <aliasgharkarani125@hotmail.com>
2020-10-04 14:14:24 -0700

    Feat/Add firebase SignIN

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
+import withFirebaseAuth from 'react-with-firebase-auth'
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import { firebaseConfig } from './firebaseConfig';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+class App extends Component {
+
+  signInWithEmailPassword = () => {
+    firebase.auth().signInWithEmailAndPassword('admin@gmail.com', 'admin123').then(function (result) {
+      console.log(result.user.email);
+    }).catch(function (error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorMessage);
+    });
+  }
+  render() {
+    // const {
+    //   user,
+    //   signOut,
+    //   signInWithGoogle,
+    // } = this.props;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {/* {
+            user
+              ? <p>Hello, {user.displayName}</p>
+              : <p>Please sign in.</p>
+          }
+
+          {
+            user
+              ? <button onClick={signOut}>Sign out</button>
+              : <button onClick={signInWithGoogle}>Sign in with Google</button>
+          } */}
+          <button onClick={this.signInWithEmailPassword}>SIGN IN</button>
+        </header>
+      </div>
+    );
+  }
 }
 
-export default App;
+const firebaseAppAuth = firebaseApp.auth();
+
+const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider(),
+};
+
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth,
+})(App);
---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyD-Y9kql8iwEEZUCdOBGv7uPr6YEE7AFCM",
+    authDomain: "react-firebase-f3349.firebaseapp.com",
+    databaseURL: "https://react-firebase-f3349.firebaseio.com",
+    projectId: "react-firebase-f3349",
+    storageBucket: "react-firebase-f3349.appspot.com",
+    messagingSenderId: "803123661467",
+    appId: "1:803123661467:web:99769bfa5069fff293143e"
+  };
